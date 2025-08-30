649f4e14a13df09883c16b0e8e81ced71aa56a2b
everfid-ever <166227111+everfid-ever@users.noreply.github.com>
2025-08-30 19:47:19 +0800

    保存

--- internal/auth/server/handler/token.go
+++ internal/auth/server/handler/token.go
@@ -9,6 +9,7 @@ import (
 	"net/url"
 	"strings"
 	"time"
+
 	"trpc.group/trpc-go/trpc-mcp-go/internal/auth"
 	"trpc.group/trpc-go/trpc-mcp-go/internal/auth/pkce"
 	"trpc.group/trpc-go/trpc-mcp-go/internal/auth/server"
