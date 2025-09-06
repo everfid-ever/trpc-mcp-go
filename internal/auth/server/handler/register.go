6e8ceb782b40617d3b661feb4470d31399736df8
everfid-ever <166227111+everfid-ever@users.noreply.github.com>
2025-09-07 04:27:36 +0800

    保存

--- internal/auth/server/handler/register.go
+++ internal/auth/server/handler/register.go
@@ -1,3 +1,9 @@
+// Tencent is pleased to support the open source community by making trpc-mcp-go available.
+//
+// Copyright (C) 2025 Tencent.  All rights reserved.
+//
+// trpc-mcp-go is licensed under the Apache License Version 2.0.
+
 package handler
 
 import (
